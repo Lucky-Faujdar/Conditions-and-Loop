b36c6bcb332c2b65b061bc6c03bb8553595473a4
Lucky-Faujdar <Lucky114005@gmail.com>
2024-08-30 13:29:12 +0530

    First commit

---
+++ Q4.js
@@ -0,0 +1,7 @@
+const num1=10;
+const num2=25;
+let i=num1+1;
+while(i<=num2){
+     console.log(i)
+     i++
+}
---
+++ Q5.js
@@ -0,0 +1,11 @@
+let i=1; //Ascending order
+while(i<=25){
+    console.log(i);
+    i++;
+}
+
+let j=25; // Descending order
+while(j>=1){
+    console.log(j);
+    j--;    
+}
